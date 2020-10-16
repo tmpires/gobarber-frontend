5b1c288c5c6b4a84f6d2c8b853d90f9183d6adfb
Thais Pires <tmendespires@gmail.com>
2020-10-16 15:51:10 -0300

    first commit

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,84 @@
+import styled, { css } from 'styled-components';
+
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  hasError: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #232129;
+  border-radius: 10px;
+  padding: 16px;
+  width: 100%;
+
+  border: 2px solid #232129;
+  color: #666360;
+
+  display: flex;
+  align-items: center;
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${(props) =>
+    props.hasError &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${(props) =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+      color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+
+  input {
+    flex: 1;
+    border: 0;
+    background: transparent;
+    color: #f4ede8;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  svg {
+    margin-right: 16px;
+    color: #666360;
+    height: 20px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background-color: #c53030;
+    color: #fff;
+    &::before {
+      content: '';
+      border-style: solid;
+      border-color: #c53030 transparent;
+      border-width: 6px 6px 0 6px;
+      top: 100%;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+`;
---
+++ src/routes/Route.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  RouteProps as DomProps,
+  Route as DomRoute,
+  Redirect,
+} from 'react-router-dom';
+import { useAuth } from '../hooks/AuthContext';
+
+interface RouteProps extends DomProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+  return (
+    <DomRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+          <Redirect
+            to={{
+              pathname: isPrivate ? '/' : '/dashboard',
+              state: { from: location },
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default Route;
